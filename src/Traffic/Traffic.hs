ebf16a3ec44c2c994b06a6c5452fb2bbb509a883
Marco Perone <pasafama@gmail.com>
2024-01-12 18:09:58 +0100

    have two pins blink in alternation

--- src/Traffic/Traffic.hs
+++ src/Traffic/Traffic.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE DataKinds #-}
 
 module Traffic.Traffic (traffic) where
 
-import Copilot.Arduino (arduino, constant, MilliSeconds (MilliSeconds), (=:), blinking, led, delay, IO, Monad ((>>)), Num (fromInteger), ($))
+import Copilot.Arduino (arduino, constant, MilliSeconds (MilliSeconds), (=:), delay, IO, Monad ((>>)), Num (fromInteger), ($), Int, Bool (True, False), Stream, replicate, (++), not)
+import Copilot.Arduino.Uno (pin2, pin3)
 
 traffic :: IO ()
 traffic = arduino $ do
-  led =: blinking
-  delay =: MilliSeconds (constant 100)
+  pin2 =: longBlinking 4
+  pin3 =: not (longBlinking 4)
+  delay =: MilliSeconds (constant 1000)
+
+longBlinking :: Int -> Stream Bool
+longBlinking n = lb
+  where lb = replicate n False ++ replicate n True ++ lb
